09f3b63ef0f256730811d52edf6b39a97197e63d
Nikita Klishin <n.klishin@oblakogroup.ru>
2023-12-17 22:27:49 +0300

    app

--- api/index.py
+++ api/index.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import keras.models
+from keras.preprocessing.image import array_to_img
+import re
+from io import BytesIO
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
+model = keras.models.load_model("data/model.h5")
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('uploader.html') 
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/recognize', methods=['POST'])
+def recognize():
+    img_size = (28, 28)
+    
+    data = BytesIO(request.files.get('file').read())
+    image = Image.open(data).resize(img_size).convert('L')
+     
+    
+    image_array = np.array(image, dtype='float32')
+    image_array = image_array/ 255.0
+
+    image_array = np.expand_dims(image_array, axis=-1)
+    image_array = np.expand_dims(image_array, axis=0)
+
+    predict = model.predict(image_array)
+
+    print('predict', np.argmax(predict))
+    
+    return jsonify(predict=str(np.argmax(predict)))
